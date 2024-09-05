1a84b13d464bad197c7a93619c6e624f1cc17be7
unknown <juliagonellaoliveira@gmail.com>
2024-09-05 16:13:13 -0300

    Criando projeto pt.2

---
+++ aula_git/src/main/java/com/ifsp/aula_git/AulaGitApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.aula_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaGitApplication.class, args);
+	}
+
+}
---
+++ aula_git/src/test/java/com/ifsp/aula_git/AulaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.aula_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
